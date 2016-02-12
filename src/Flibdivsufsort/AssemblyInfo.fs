e4d4baf2a080e369f89b3077280d57e4b2ad4fd0
Rick Minerich <richard.minerich@gmail.com>
2016-02-12 17:20:34 -0500

    Initial Project Scaffold and compiled Libdivsufsort dll

---
+++ src/Flibdivsufsort/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Flibdivsufsort")>]
+[<assembly: AssemblyProductAttribute("Flibdivsufsort")>]
+[<assembly: AssemblyDescriptionAttribute("An F# Wrapper around the LibDibSufSort suffix array library")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
